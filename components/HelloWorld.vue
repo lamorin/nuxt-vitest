0b3f393324411957c12d54abbbb2938c86c0e21e
lamorin <luis.morin@gmail.com>
2023-05-31 12:16:13 -0400

    Nuxt 3.0 project, vitest configured (uses ESLint)

---
+++ components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Component: HelloWorld
+  </div>
+</template>
+
+<style scoped></style>
---
+++ tests/HelloWorld.spec.ts
@@ -0,0 +1,12 @@
+// tests/HelloWorld.spec.ts
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import HelloWorld from '../components/HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('is a Vue instance', () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
